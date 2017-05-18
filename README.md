dada79f0848c3b9bc1858c72adc4232c30626946
Theódór Magnússon <teddi@menn.is>
2017-05-18 21:34:18 +0000

    js dom injection and minor styling

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # StaticBase
+A static implementation of an http get call to a firebase database.
+Using ES6's fetch function.
+
+```
+https://staticbasetest.firebaseio.com/.json
+```
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // PURE JS FIREBASE API CALL
-const url = 'https://staticbasetest.firebaseio.com/.json';
-fetch(url).then((response) => {
-    response.json().then((data) => {
-        console.log(data);
+window.onload = function() {
+    const url = 'https://staticbasetest.firebaseio.com/.json';
+    const countries = document.querySelector('.countries');
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            //data => json object fetches from url.
+            for(let key in data){
+                let li = document.createElement('li');
+                li.innerHTML = `
+                    <img
+                        src="https://lipis.github.io/flag-icon-css/flags/4x3/${key.toLowerCase()}.svg"
+                        alt="${data[key]}"
+                    />
+
+                    <h2> ${key} ${data[key]}</h2>
+                `;
+                countries.append(li);
+            }
+        });
     });
-});
+};
+
+
